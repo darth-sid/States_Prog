79f425e5b88cd9af55d6bcad2465ea9aa9ec54e8
darth-sid <55854690+darth-sid@users.noreply.github.com>
2022-03-03 23:56:08 -0800

    auton kinda

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,28 @@
 //================================================================================================================
 
 //PORTS===========================================================================================================
-#define DRIVELEFT1PORT 9
-#define DRIVELEFT2PORT 10
-#define DRIVELEFT3PORT 18
-#define DRIVELEFT4PORT 20
-#define DRIVERIGHT1PORT 7
-#define DRIVERIGHT2PORT 8
-#define DRIVERIGHT3PORT 19
-#define DRIVERIGHT4PORT 17
+#define DRIVELEFT1PORT 18//ok
+#define DRIVELEFT2PORT 6//rev
+#define DRIVELEFT3PORT 7//rev
+#define DRIVELEFT4PORT 8//ok
+#define DRIVERIGHT1PORT 10//rev
+#define DRIVERIGHT2PORT 9//ok
+#define DRIVERIGHT3PORT 3//ok
+#define DRIVERIGHT4PORT 1//rev
 
-#define PNEUM_PORT_1 1
-#define PNEUM_PORT_2 2
-#define PNEUM_PORT_3 3
+#define LEFT_ROT_PORT 20
+#define RIGHT_ROT_PORT 13
+#define BACK_ROT_PORT 14
+
+#define PNEUM_PORT_1 6
+#define PNEUM_PORT_2 5
+
+#define INERTIAL_PORT 12
+
+#define ENC_TO_INCH (2.75*M_PI/36000)
+
+#define TRACKER_DISTANCE 3.375
+#define TRACKER_DISTANCE_BACK 1
 //UI==============================================================================================================
 //buttons
 lv_obj_t * autonButton;
@@ -43,8 +53,8 @@ pros::Motor driveLeft1(DRIVELEFT1PORT , MOTOR_GEARSET_06, false);
 pros::Motor driveLeft2(DRIVELEFT2PORT , MOTOR_GEARSET_06, true);
 pros::Motor driveLeft3(DRIVELEFT3PORT , MOTOR_GEARSET_06,true);
 pros::Motor driveLeft4(DRIVELEFT4PORT , MOTOR_GEARSET_06,false);
-pros::Motor driveRight1(DRIVERIGHT1PORT , MOTOR_GEARSET_06, false);
-pros::Motor driveRight2(DRIVERIGHT2PORT , MOTOR_GEARSET_06, true);
+pros::Motor driveRight1(DRIVERIGHT1PORT , MOTOR_GEARSET_06, true);
+pros::Motor driveRight2(DRIVERIGHT2PORT , MOTOR_GEARSET_06, false);
 pros::Motor driveRight3(DRIVERIGHT3PORT , MOTOR_GEARSET_06,false);
 pros::Motor driveRight4(DRIVERIGHT4PORT , MOTOR_GEARSET_06,true);
 
@@ -52,8 +62,15 @@ pros::Controller Vcontroller(pros::E_CONTROLLER_MASTER);
 
 pros::ADIDigitalOut FrontClamp(PNEUM_PORT_1);
 pros::ADIDigitalOut BackClamp(PNEUM_PORT_2);
-pros::ADIDigitalOut SideClamp(PNEUM_PORT_3);
+
+pros::Rotation left_rot(LEFT_ROT_PORT);
+pros::Rotation right_rot(RIGHT_ROT_PORT);
+pros::Rotation back_rot(BACK_ROT_PORT);
 //MISC===========================================================================================================
+float tare_right;
+float tare_left;
+float tare_back;
+
 char text1[100];
 std::map<int,std::string> auton_names;
 int auton = 0;
@@ -63,6 +80,14 @@ bool p1 = false;
 bool p2 = false;
 bool p3 = false;
 
+float prev_y_encoder = 0;
+float prev_x_encoder = 0;
+float prev_angle = 0;
+
+//GLOBAL X
+float pos_x = 0;
+//GLOBAL Y
+float pos_y = 0;
 //FUNCS===========================================================================================================
 static lv_res_t btn_action(lv_obj_t * btn) {
 	int btn_id = lv_obj_get_free_num(btn);
@@ -91,7 +116,7 @@ void setDriveMotors(int l, int r) {
 void setDrive() {
 	int leftJoystickYInput = Vcontroller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 	int leftJoystickXInput = Vcontroller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);//*dir;
-
+	pros::lcd::set_text(1, std::to_string(leftJoystickYInput));
   //Deadzones (10,10)
   if (abs(leftJoystickYInput) < 10) {
     leftJoystickYInput = 0;
@@ -99,22 +124,19 @@ void setDrive() {
   if (abs(leftJoystickXInput) < 10) {
     leftJoystickXInput = 0;
   }
+
   setDriveMotors((leftJoystickYInput+leftJoystickXInput), (leftJoystickYInput+(-leftJoystickXInput)));
 	//setDrive (leftJoystickYInput, rightJoystickYInput);
 }
 
 void clamps() {
-	if(Vcontroller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)){
+	if(Vcontroller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)){
 		p1 = !p1;
 		FrontClamp.set_value(p1);
 	}
-	if(Vcontroller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)){
+	if(Vcontroller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)){
 		p2 = !p2;
 		BackClamp.set_value(p2);
-	}
-	if(Vcontroller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)){
-		p3 = !p3;
-		SideClamp.set_value(p3);
 	}
 }
 
@@ -149,8 +171,108 @@ void brake_brake(){
 	driveRight4.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
 }
 
+//scuffed rot sensor stuff
+void tare_rot(){
+	while (tare_right == 0){
+		tare_right = right_rot.get_position();
+		pros::delay(1);
+	}
+	while (tare_left == 0){
+		tare_left = left_rot.get_position();
+		pros::delay(1);
+	}
+	while (tare_back == 0){
+		tare_back = back_rot.get_position();
+		pros::delay(1);
+	}
+}
+
+float right_rot_pos(){
+ 	return right_rot.get_position() - tare_right;
+ }
+float left_rot_pos(){
+ 	return left_rot.get_position() - tare_left;
+ }
+float back_rot_pos(){
+	return back_rot.get_position() - tare_back;
+}
+
+double getAngle(){
+	double TRACKER_SCALAR = 1.057;
+	double encoder_angle = (right_rot_pos()*2.75*M_PI/36000 - left_rot_pos()*2.75*M_PI/36000)/(TRACKER_DISTANCE)*TRACKER_SCALAR;
+  //pros::lcd::set_text(1, std::to_string(inertial.get_yaw()));
+  return encoder_angle;
+  //return encoder_angle;
+}
+
+void updateCoords(){
+	double angle = getAngle();
+	double delta_angle = angle-prev_angle;
+	//tracked values in inches(ideal centidegrees to inches conversion: 2.75*pi/36000)
+	double curr_y_encoder = (left_rot_pos()+right_rot_pos())/2;
+	double curr_x_encoder = back_rot_pos();
+	int delta_local_y = (curr_y_encoder-prev_y_encoder);
+	int delta_local_x = (curr_x_encoder-prev_x_encoder);// - TRACKER_DISTANCE_BACK*getAngle();
+	int distance = sqrt(pow(delta_local_y,2)+pow(delta_local_x,2));
+	//account for backwards movement
+	if(delta_local_y < 0) angle += M_PI;
+	float adjustment = (std::isnan(atan((delta_local_x)/(delta_local_y)))) ? M_PI/2 : atan((delta_local_x)/(delta_local_y));
+	pros::lcd::set_text(1, std::to_string((angle-adjustment)));
+	pos_x += cos(angle - adjustment)*distance*ENC_TO_INCH;
+	pos_y += sin(angle - adjustment)*distance*ENC_TO_INCH;
+	prev_x_encoder = curr_x_encoder;
+	prev_y_encoder = curr_y_encoder;
+	prev_angle = angle;
+}
+
+void setAngle(double target, double tolerance, double kP, double kI, double kD){
+	brake_hold();
+	double preverror = 0;
+	double error = target-getAngle()*180/M_PI;
+	double integral = 0;
+
+	while (fabs(error) > tolerance){
+		error = target-getAngle()*180/M_PI;
+		double derivative = error-preverror;
+		double pwr = error*kP + integral*kI + derivative*kD;
+		setDriveMotors(pwr, -pwr);
+		pros::lcd::set_text(1, std::to_string(error));
+		preverror = error;
+		integral += error;
+		pros::delay(10);
+	}
+	setDriveMotors(0,0);
+	brake_coast();
+}
+
+void drive(double targetx, double targety, double tolerance, double kP, double kI, double kD){
+	double target_angle = atan(targety/targetx);
+	//tune constants
+	setAngle(target_angle,1,1,0,0);
+
+	brake_hold();
+	double preverror = 0;
+	double error = sqrt(pow(targety,2)+pow(targetx,2));
+	double integral = 0;
+	while(fabs(error)>tolerance){
+		error = sqrt(pow(targety,2)+pow(targetx,2));
+		double derivative = error-preverror;
+		double pwr = error*kP + integral*kI + derivative*kD;
+		setDriveMotors(pwr,pwr);
+		pros::lcd::set_text(1, std::to_string(error));
+		preverror = error;
+		integral += error;
+		pros::delay(10);
+	}
+	setDriveMotors(0,0);
+	brake_coast();
+}
+
+
 void initialize() {
-	change=true;
+	pros::lcd::initialize();
+	tare_rot();
+	/*change=true;
 	auton_names.insert(std::make_pair(1,"auton1"));
 	auton_names.insert(std::make_pair(2,"auton2"));
 	auton_names.insert(std::make_pair(3,"auton3"));
@@ -183,7 +305,7 @@ void initialize() {
 
 	selectionLabel = lv_label_create(lv_scr_act(), NULL); //create label and puts it on the screen
 	lv_label_set_text(selectionLabel, "None Selected"); //sets label text
-	lv_obj_align(selectionLabel, NULL, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_align(selectionLabel, NULL, LV_ALIGN_CENTER, 0, 0);*/
 }
 
 void disabled() {}
@@ -191,13 +313,17 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
-	change=false;
-	if(auton==0) auton=1;
-	sprintf(text1,"a%01d",auton);
-	lv_label_set_text(selectionLabel, text1);
+	setAngle(180,1,1.2,0,0);
 }
 
 void opcontrol() {
-	setDrive();
-	clamps();
+	while(true){
+		setDrive();
+		clamps();
+		pros::lcd::set_text(1, std::to_string(getAngle()*180/M_PI));
+		pros::lcd::set_text(2, "y: " + std::to_string(pos_y));
+		pros::lcd::set_text(3, "x: " + std::to_string(pos_x));
+		updateCoords();
+		pros::delay(10);
+	}
 }
