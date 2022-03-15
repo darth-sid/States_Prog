a08442aa1b07a99e22f1ef1e772b3a9cac418a45
darth-sid <55854690+darth-sid@users.noreply.github.com>
2022-03-14 21:50:45 -0700

    turny shi in progress

--- src/main.cpp
+++ src/main.cpp
@@ -28,9 +28,14 @@
 #define INERTIAL_PORT 12
 
 #define ENC_TO_INCH (2.75*M_PI/36000)
+#define DEG_TO_RAD M_PI/180
 
 #define TRACKER_DISTANCE 3.375
 #define TRACKER_DISTANCE_BACK 1
+
+#define MAX_VEL 127.0
+#define TURN_VEL_K 1
+#define T 16
 //UI==============================================================================================================
 //buttons
 lv_obj_t * autonButton;
@@ -91,6 +96,7 @@ float pos_x = 0;
 //GLOBAL Y
 float pos_y = 0;
 //FUNCS===========================================================================================================
+//auton selector [WIP]
 static lv_res_t btn_action(lv_obj_t * btn) {
 	int btn_id = lv_obj_get_free_num(btn);
 	if(change){
@@ -130,7 +136,7 @@ void setDrive() {
   setDriveMotors((leftJoystickYInput+leftJoystickXInput), (leftJoystickYInput+(-leftJoystickXInput)));
 	//setDrive (leftJoystickYInput, rightJoystickYInput);
 }
-
+//trigger clamps on button events
 void clamps() {
 	if(Vcontroller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)){
 		p1 = !p1;
@@ -142,6 +148,7 @@ void clamps() {
 	}
 }
 
+//brakemodes .-.
 void brake_coast(){
 	driveLeft1.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	driveLeft2.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
@@ -188,25 +195,24 @@ void tare_rot(){
 		pros::delay(1);
 	}
 }
-
 float right_rot_pos(){
  	return right_rot.get_position() - tare_right;
- }
+}
 float left_rot_pos(){
  	return left_rot.get_position() - tare_left;
- }
+}
 float back_rot_pos(){
 	return back_rot.get_position() - tare_back;
 }
 
 double getAngle(){
 	double TRACKER_SCALAR = 1.057;
-	double encoder_angle = (right_rot_pos()*2.75*M_PI/36000 - left_rot_pos()*2.75*M_PI/36000)/(TRACKER_DISTANCE)*TRACKER_SCALAR;
-  //pros::lcd::set_text(1, std::to_string(inertial.get_yaw()));
-  return inertial.get_rotation();//encoder_angle;
+	double encoder_angle = ((right_rot_pos()*ENC_TO_INCH - left_rot_pos()*ENC_TO_INCH)/(TRACKER_DISTANCE))*TRACKER_SCALAR;
+  return inertial.get_rotation();
   //return encoder_angle;
 }
 
+//odom :D
 void updateCoords(){
 	double angle = getAngle();
 	double delta_angle = angle-prev_angle;
@@ -218,7 +224,8 @@ void updateCoords(){
 	int distance = sqrt(pow(delta_local_y,2)+pow(delta_local_x,2));
 	//account for backwards movement
 	if(delta_local_y < 0) angle += M_PI;
-	float adjustment = (std::isnan(atan((delta_local_x)/(delta_local_y)))) ? M_PI/2 : atan((delta_local_x)/(delta_local_y));
+	double adjustment = atan((delta_local_x)/(delta_local_y));
+	adjustment = (std::isnan(adjustment)) ? M_PI/2 : adjustment;
 	pros::lcd::set_text(1, std::to_string((angle-adjustment)));
 	pos_x += cos(angle - adjustment)*distance*ENC_TO_INCH;
 	pos_y += sin(angle - adjustment)*distance*ENC_TO_INCH;
@@ -300,6 +307,51 @@ void drive(double targetx, double targety, double tolerance, double kP, double k
 void drive(double targetx, double targety, double tolerance, double kP, double kI, double kD){
 	drive(targetx,targety,tolerance,kP,kI,kD,100000);
 }
+
+//weeeeee nonsense
+float curv(float pts[][2], int pti){
+	float x1 = pts[pti-1][0];
+	float y1 = pts[pti-1][1];
+  x1 += 0.0000001;
+  float x2 = pts[pti][0];
+	float y2 = pts[pti][1];
+	float x3 = pts[pti+1][0];
+	float y3 = pts[pti+1][1];
+  float k1  = 0.5*(x1*x1-x2*x2+y1*y1-y2*y2)/(x1-x2);
+  float k2 = (y1-y2)/(x1-x2);
+  float b = 0.5*(x2*x2 - 2*x2*k1 + y2*y2 - x3*x3 + 2*x3*k1 - y3*y3)/(x3*k2 - y3 + y2 - x2*k2);
+  float a = k1-k2*b;
+  float r_sq = (x1-a)*(x1-a) + (y1-b)*(y1-b);
+  float r = pow(r_sq,0.5);
+  return 1/r;
+}
+
+void driveTurn(double x, double y, double end_angle, double tolerance, double kP, double kI, double kD, double time){
+
+	x += 0.000000001;
+	float d = sqrt(pow(x-pos_x,2)+pow(y-pos_y,2));
+	float dx = cos(atan((y-pos_y)/(x-pos_x))+getAngle()*DEG_TO_RAD)*d;
+	float c = (2*dx)/pow(d,2);
+	float pts[][2] = {{pos_x,pos_y},{x,y},{x*(1+cos(end_angle)),y*(1+sin(end_angle))}};
+	float v = std::min((float)MAX_VEL, TURN_VEL_K/curv(pts,1));
+	float l = v*(2+c*T)/2;
+	float r = v*(2+c*T)/2;
+	while(true){
+		pros::lcd::set_text(1, "l:" + std::to_string(l));
+		pros::lcd::set_text(2, "r:" + std::to_string(r));
+		pros::lcd::set_text(3, "atan:" + std::to_string(atan((y-pos_y)/(x-pos_x))));
+		pros::lcd::set_text(4, "O:" + std::to_string(getAngle()*DEG_TO_RAD));
+		pros::lcd::set_text(5, "dx:" + std::to_string(dx));
+		pros::lcd::set_text(6, "c:" + std::to_string(c));
+	}
+
+
+
+}
+
+//vel = min(MAX_VEL,TURN_VEL_K/curv(i))
+//nonsense ends
+
 //side mogo
 void auton1(){
 	FrontClamp.set_value(false);
@@ -407,7 +459,7 @@ void competition_initialize() {}
 
 void autonomous() {
 
-	auton3();
+	driveTurn(10, -10, 45, 1, 0, 0, 0, 0);
 	//pidref:
 	//setAngle(-180, 0.1, 50, 0, 300, 5000);
 	//drive(200000,0,500,0.015,0,0.082,5000);
